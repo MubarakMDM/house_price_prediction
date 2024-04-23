d05e49aaf88db55ba3c5be2a823391f06500484e
MubarakMDM <md93150@gmail.com>
2024-04-23 13:50:07 +0200

    all files

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# house_price_prediction
+# PricePrediction
+### Software and Tools Requirements
+
+1. [Github Account](https://github.com)
+2. [HerokuAccount](https://heroku.com)
+3. [VSCodeIDE](https://code.visualstudio.com/)
+4. [GitCLI](https://git-scm.com/book/en/v2)
+
+Create a new environment 
+
+
+'''
+#to download file from web to local machine
+git clone url
+
+conda create -p env python==3.7 -y
+conda activate env/
+'''
+
+#Crate requirements.txt file 
+'''
+pip install -r requirements.txt
+'''
+
+#push the code Gid repository
+'''
+git add requirements.txt (to add single file )OR  git add . (to add all files)
+git status (to see the status)
+git commit -m "This is my first comit which includes 3 files" (to commit the files to git hub)
+git config --global user.email "md93150@gmail.com"
+git config --global user.name "MubarakMDM"
+git push origin main
+'''
+
+
+# github copilet gives code inteligence, have to install this extention
---
+++ app.py
@@ -0,0 +1,40 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for,render_template
+import numpy as np
+import pandas as pd
+
+# Starting point of app
+app = Flask(__name__)
+
+# Load the model
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
+
+#got to home page
+@app.route('/')
+#here is the home page, once we hit his app this goes to home.html page
+def home():
+    return render_template('home.html')
+
+# POST : send a request and get the output
+@app.route('/predict_api', methods = ['POST'])
+#here is hte definition of predict_api
+def predict_api():
+    #we will give 'data' as inpout in json formate and result will be stored in dataValue
+    dataValue = request.json['data']
+    print(dataValue)
+    print(np.array(list(dataValue.values())).reshape(1,-1))
+    new_data = scalar.trasform(np.array(list(dataValue.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data =  [float(x) for x in request.form.values()]
+    final_input = scalar.trasform(np.array(data.reshape(1,-1)))
+
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
